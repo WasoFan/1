dd18b7d9ff72fce8875c0e95b0208f682b1e45e2
29569 <2956901631@qq.com>
2023-03-10 18:17:11 +0800

    第一次提交

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("你好");
+        }
+    }
+}
